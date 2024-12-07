993fb6b5a1be8ab5012ef38350dfade932d7f724
SokLeangSiv <lsok@paragoniu.edu.kh>
2024-12-08 00:29:04 +0700

    vattanak

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'narak',
+      title: 'vattanak is not here',
       theme: ThemeData(
         primarySwatch: Colors.yellow,
         scaffoldBackgroundColor: Colors.yellow[50],
