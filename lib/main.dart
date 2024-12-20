40232277ffa236172813e304a955c044972725e1
SokLeangSiv <lsok@paragoniu.edu.kh>
2024-12-20 22:19:38 +0700

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,180 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:midterm/components/notes.dart';
-import 'package:midterm/pages/edit_todo.dart';
+import 'package:midterm/pages/home_page.dart';
 
-class NoteDetail extends StatelessWidget {
-  final Notes note;
-  const NoteDetail({
-    super.key,
-    required this.note,
-  });
+import 'loading_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Convert the color string to a Flutter Color object
-    final Color noteColor = stringToColor(note.color);
-
-    return Scaffold(
-      backgroundColor: noteColor, // Set the background color of the Scaffold
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => EditTodo(note: note)),
-              );
-            },
-            icon: const Icon(Icons.edit),
-          ),
-        ],
-        title: const Text(
-          'Note Detail',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            color: noteColor, // Set the color of the AppBar
-          ),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'vattanak is not here',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        scaffoldBackgroundColor: Colors.yellow[50], // body background color
+        appBarTheme: const AppBarTheme( // pre-design for appbar
+          backgroundColor: Colors.yellow,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          )
+        )
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            NoteSection(
-              title: 'Title',
-              content: note.title.toString(),
-              icon: Icons.title_outlined,
-            ),
-            const SizedBox(height: 20),
-            NoteSection(
-              title: 'Description',
-              content: note.description.toString(),
-              icon: Icons.description,
-            ),
-            const SizedBox(height: 20),
-            NoteSection(
-              title: 'Date',
-              content: note.date.toString(),
-              icon: Icons.calendar_today,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Color',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              height: 80,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: noteColor, // Set the container's color dynamically
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 8,
-                    offset: Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Text(
-                  note.color.toString(),
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: noteColor.computeLuminance() > 0.5
-                        ? Colors.black
-                        : Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// Helper function to convert a color string to a Color object
-Color stringToColor(String colorString) {
-  // Assuming the color string is in hex format (e.g., "#FF5733" or "FF5733")
-  final buffer = StringBuffer();
-  if (colorString.length == 6 || colorString.length == 7) buffer.write('ff'); // Add opacity if not provided
-  buffer.write(colorString.replaceFirst('#', ''));
-  return Color(int.parse(buffer.toString(), radix: 16));
-}
-
-class NoteSection extends StatelessWidget {
-  final String title;
-  final String content;
-  final IconData icon;
-
-  const NoteSection({
-    super.key,
-    required this.title,
-    required this.content,
-    required this.icon,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      elevation: 4,
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Icon(
-              icon,
-              size: 30,
-              color: Colors.blueAccent,
-            ),
-            const SizedBox(width: 15),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    content,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.black54,
-                    ),
-                    softWrap: true,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 5,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: LoadApp() ,
     );
   }
 }
--- lib/pages/note_detail.dart
+++ lib/pages/note_detail.dart
@@ -3,7 +3,6 @@ import 'package:midterm/components/notes.dart';
 import 'package:midterm/pages/edit_todo.dart';
 
 class NoteDetail extends StatelessWidget {
-  // final Map<String, dynamic> note;
   final Notes note;
   const NoteDetail({
     super.key,
@@ -12,18 +11,21 @@ class NoteDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Convert the color string to a Flutter Color object
+    final Color noteColor = stringToColor(note.color ?? '#FFFFFF');
+
     return Scaffold(
+      backgroundColor: noteColor, // Set the background color of the Scaffold
       appBar: AppBar(
         actions: [
           IconButton(
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => EditTodo(note: note)) // parameter page
+                MaterialPageRoute(builder: (context) => EditTodo(note: note)),
               );
             },
-            icon: Icon(Icons.edit),
-
+            icon: const Icon(Icons.edit),
           ),
         ],
         title: const Text(
@@ -34,8 +36,8 @@ class NoteDetail extends StatelessWidget {
           ),
         ),
         flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            color: Colors.yellow,
+          decoration: BoxDecoration(
+            color: noteColor, // Set the color of the AppBar
           ),
         ),
       ),
@@ -46,21 +48,18 @@ class NoteDetail extends StatelessWidget {
           children: [
             NoteSection(
               title: 'Title',
-              // content: note['title'],
               content: note.title.toString(),
               icon: Icons.title_outlined,
             ),
             const SizedBox(height: 20),
             NoteSection(
               title: 'Description',
-              // content: note['subtitle'],
               content: note.description.toString(),
               icon: Icons.description,
             ),
             const SizedBox(height: 20),
             NoteSection(
               title: 'Date',
-              // content: note['date'],
               content: note.date.toString(),
               icon: Icons.calendar_today,
             ),
@@ -78,7 +77,7 @@ class NoteDetail extends StatelessWidget {
               height: 80,
               width: double.infinity,
               decoration: BoxDecoration(
-                // color: note['color'],
+                color: noteColor, // Set the container's color dynamically
                 borderRadius: BorderRadius.circular(15),
                 boxShadow: const [
                   BoxShadow(
@@ -93,7 +92,7 @@ class NoteDetail extends StatelessWidget {
                   note.color.toString(),
                   style: TextStyle(
                     fontSize: 18,
-                    color: stringToColor(note.color).computeLuminance() > 0.5
+                    color: noteColor.computeLuminance() > 0.5
                         ? Colors.black
                         : Colors.white,
                     fontWeight: FontWeight.bold,
@@ -106,6 +105,15 @@ class NoteDetail extends StatelessWidget {
       ),
     );
   }
+}
+
+// Helper function to convert a color string to a Color object
+Color stringToColor(String colorString) {
+  // Assuming the color string is in hex format (e.g., "#FF5733" or "FF5733")
+  final buffer = StringBuffer();
+  if (colorString.length == 6 || colorString.length == 7) buffer.write('ff'); // Add opacity if not provided
+  buffer.write(colorString.replaceFirst('#', ''));
+  return Color(int.parse(buffer.toString(), radix: 16));
 }
 
 class NoteSection extends StatelessWidget {
@@ -158,8 +166,8 @@ class NoteSection extends StatelessWidget {
                       color: Colors.black54,
                     ),
                     softWrap: true,
-                    overflow: TextOverflow.ellipsis, // Prevent overflow
-                    maxLines: 5, // Adjust number of lines to display
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 5,
                   ),
                 ],
               ),
@@ -170,4 +178,3 @@ class NoteSection extends StatelessWidget {
     );
   }
 }
-
